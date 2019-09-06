545478c269a97f5fdfb730f49278c83a2d108deb
leswing <lilleswing@gmail.com>
2019-09-06 11:58:41 -0400

    no hard-coded cookie

--- get_crossword.py
+++ get_crossword.py
@@ -53,6 +53,6 @@ def main(cookie):
     
 
 if __name__ == "__main__":
-    'nyt-a=iO-KNrTLeKmrjNco0vzP52; vistory=a00; vi_www_hp=b20; optimizelyEndUserId=oeu1546533760629r0.2615924416086872; edu_cig_opt=%7B%22isEduUser%22%3Afalse%7D; b2b_cig_opt=%7B%22isCorpUser%22%3Afalse%7D; nyt-gdpr=0; nyt-purr=cfh; NYT-T=ok; nyt-geo=US; NYT-MPS=fd6f3900b6584cdd06a68c46a19e8b6e828a49d3d49e2447b90c24f1ddce2e70377c1766f88cfc63f11ea3592913e508; nyt-d=101.000000000NAI00000s9Iny1%2F6ouh0pA2e%2F1w9pKn0t8dnq1Z0XO70I5Xjy05A34e0zUWSA0U50eC1vA3Ch0rBdyR0J6nyJ07PGT%2F0R4niB0K1WS800PNDo1fUNPW1mVMD1%40a948c6db%2Fbf37799d; NYT-S=1ETWFtaOz.A/a1PmDaDQ3oAmgWLxQuZyROCbboExJAXx.fW.Z.J3Nzm6wl1qsSIblfalTpzrtSHSFRbebnCk3bSqcf/s/hPYX4XBXqwlZsucOa0qNORN2C/oCUXGnqS1ypLzVvSpZreYg0; nyt-auth-method=sso'
+    
     cookie = sys.argv[1]
     main(cookie)
